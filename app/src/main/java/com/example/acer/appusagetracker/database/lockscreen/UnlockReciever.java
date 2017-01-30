e2d0765e8614a6d1bb3fd620e5996a599bf112f0
RohitAntil <rohit.ntil@gmail.com>
2017-01-30 15:42:52 +0530

    LockScreen DB added

--- app/src/main/java/com/example/acer/appusagetracker/database/lockscreen/UnlockReciever.java
+++ app/src/main/java/com/example/acer/appusagetracker/database/lockscreen/UnlockReciever.java
@@ -1,10 +1,14 @@
 package com.example.acer.appusagetracker.database.lockscreen;
 
 import android.app.KeyguardManager;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
+import com.example.acer.appusagetracker.R;
 
 /**
  * Created by rohit02.kumar on 1/30/2017.
@@ -15,7 +19,14 @@ public class UnlockReciever extends BroadcastReceiver {
         public void onReceive(Context context, Intent intent) {
             KeyguardManager keyguardManager = (KeyguardManager)context.getSystemService(Context.KEYGUARD_SERVICE);
             if (keyguardManager.isKeyguardSecure()) {
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(context)
+                                .setSmallIcon(R.drawable.ic_default_app_launcher)
+                                .setContentTitle("App Usage")
+                                .setContentText("Phone Unlocked");
 
+                NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.notify(1, mBuilder.build());
 
             }
         }
